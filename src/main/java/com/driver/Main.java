913ffccceb0ee509b7ae99374a46fdffc6d10c3c
misterKumar <120304503+misterKumar@users.noreply.github.com>
2023-01-11 20:16:08 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,16 +1,19 @@
 package com.driver;
 
 public class Main {
-  A obj=new B();
-  System.out.println(obj.meth());
+  public void main (String[] args){
+    A obj = new B();
+    String ans= obj.meth();
+    System.out.println(ans);
+  }
 }
 class A{
   public String meth(){
-    retutn "Invoking method from class A";
+    return "Invoking method from class A";
   }
 }
 class B extends A{
-public void meth(){
-    return ;
+public String meth(){
+    return "";
   }
 }
